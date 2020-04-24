91365f931e137b7819236e7bf056de84b6f8794c
Ihor Kolodych <31418327+CuteFix@users.noreply.github.com>
2020-04-25 01:31:38 +0300

    optimization metod

--- security_system/security_system.ino
+++ security_system/security_system.ino
@@ -2,138 +2,81 @@
 #include <HTTPClient.h>
 #include "SPIFFS.h"
 #include <SPI.h>
-#include <FS.h>    
+#include <FS.h>
 #include <Ethernet.h>
-#include <MFRC522.h>   
+#include <MFRC522.h>
+#include "string"
 
-
-#define RST_PIN         4         
-#define SS_PIN          2    
-MFRC522 mfrc522(SS_PIN, RST_PIN);   
-int card,uid;
-char floatbufVar[32]; 
+#define RST_PIN         4
+#define SS_PIN          2
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+int card, uid;
+char floatbufVar[32];
 const int ledPinGreen = 15;
 const int ledPinRed = 17;
 const int ledPinYellow = 21;
- 
-const char* ssid = "WiFiId";
-const char* password =  "password";
+
+const char *ssid = "Yra";
+const char *password = "qwerr12345";
 
 void setup() {
- 
-     
-     
-  
-  delay(4000);
-     SPIFFS.begin();
-     SPI.begin();
-     Serial.begin(115200);
-     mfrc522.PCD_Init();
-     pinMode(ledPinGreen, OUTPUT);
-     pinMode(ledPinRed, OUTPUT);
-     pinMode(ledPinYellow, OUTPUT); 
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
- 
-  Serial.println("Connected to the WiFi network");
- 
-}
- 
-void loop() {
- if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
-           return;
-        }
-  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
- 
-    HTTPClient http;
-    //for (int i=0; i<=quantity_; i++){
-    http.begin("http://192.168.0.2/"); //Specify the URL
-
-     //}
-
-    int httpCode = http.GET();                                        //Make the request
-
-    if (httpCode > 0) { //Check for the returning code
-        String s = http.getString();
-        //int countint=atoi(count);
-        Serial.println(httpCode);
-        
-        Serial.println(s);
-        Serial.println("HTTP request");
-
-        int i = 0, c = 0;
-        do { c++; } while ((i = s.indexOf(',', ++i)) > 0);
-        unsigned int rawData[c];
-        i = 0;
-         c = 0;
-        int ix = 0;
-        String ts;
-        do {
-        ix = s.indexOf(',', i + 1);
-        ts = s.substring(i, ix);
-        rawData[c] = (unsigned int)ts.toInt();
-        Serial.println(rawData[c]);
-        c++;
-        } while ((i = s.indexOf(',', ++i) + 1) > 0);
-http.end(); //Free the resources
-//........................
-
-     String uid_;  
-     uid_ += mfrc522.uid.uidByte[1];     
-     uid_ += mfrc522.uid.uidByte[2];
-     uid_ += mfrc522.uid.uidByte[3];
-     uid_ += mfrc522.uid.uidByte[4];  
-
-
-        http.begin("http://192.168.0.2/count.php");
-        int httpCode = http.GET();
-        String count = http.getString();
-        Serial.println(count);
-
-     
-     uid_.toCharArray(floatbufVar,sizeof(floatbufVar));
-     card=atof(floatbufVar);
-     Serial.print("Card UID: ");
-     Serial.println(card );
-     int quantity_ = count.toInt();//количество записей в базе
-     Serial.println(quantity_);
-     for (int i=0; i<=quantity_; i++){
-     if (card == rawData[i]) {//Если  есть в базе
-         Serial.println("Open");
-         digitalWrite(ledPinGreen, HIGH);
-              delay(5000);
-              digitalWrite(ledPinGreen, LOW); 
-              i=rawData[i];
-              i++;
-              break;
-         }else {
-          digitalWrite(ledPinRed, HIGH);
-          delay(100);
-          digitalWrite(ledPinRed, LOW); 
-          Serial.println("closed");
-          
-         }
-         
-     }
-//........................
-        
-      }
- 
-    else {
-      Serial.println(http.GET());
-      Serial.println(httpCode);
-      Serial.println("Error on HTTP request");
-      String payload = http.getString();
-      Serial.println(payload);
+    delay(4000);
+    SPIFFS.begin();
+    SPI.begin();
+    Serial.begin(115200);
+    mfrc522.PCD_Init();
+    pinMode(ledPinGreen, OUTPUT);
+    pinMode(ledPinRed, OUTPUT);
+    pinMode(ledPinYellow, OUTPUT);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi..");
     }
- 
-    http.end(); //Free the resources
-  }
- digitalWrite(ledPinYellow, HIGH);
-  delay(5000);
- digitalWrite(ledPinYellow, LOW);
+    Serial.println("Connected to the WiFi network");
+}
+
+void loop() {
+    if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+        return;
+    }
+    if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+        HTTPClient http;
+        String uid_;
+        uid_ += mfrc522.uid.uidByte[1];
+        uid_ += mfrc522.uid.uidByte[2];
+        uid_ += mfrc522.uid.uidByte[3];
+        uid_ += mfrc522.uid.uidByte[4];
+        String request = "http://192.168.0.2/?uid=" + uid_;
+        Serial.println(request);
+        http.begin(request); //Specify the URL
+        int httpCode = http.GET();//Make the request
+        if (httpCode > 0) { //Check for the returning code
+            std::string s = http.getString().c_str();
+            Serial.println(httpCode);
+            Serial.println(s.c_str());
+            Serial.println("HTTP request");
+            http.end(); //Free the resources
+            if (s.find("1")!=std::string::npos) {//Если  есть в базе
+                Serial.println("Open");
+                digitalWrite(ledPinGreen, HIGH);
+                delay(5000);
+                digitalWrite(ledPinGreen, LOW);
+            } else {
+                digitalWrite(ledPinRed, HIGH);
+                delay(100);
+                digitalWrite(ledPinRed, LOW);
+                Serial.println("closed");
+            }
+        } else {
+            Serial.println(http.GET());
+            Serial.println(httpCode);
+            Serial.println("Error on HTTP request");
+            String payload = http.getString();
+            Serial.println(payload);
+        }
+    }
+    digitalWrite(ledPinYellow, HIGH);
+    delay(5000);
+    digitalWrite(ledPinYellow, LOW);
 }

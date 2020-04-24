cee28c35a710dd9b535a68e69ee02fd8f62076f2
Ihor Kolodych <31418327+CuteFix@users.noreply.github.com>
2020-04-25 01:33:22 +0300

    Delete count.php

--- qerysites/count.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'bd.php'; // подключаем скрипт
- 
-$link = mysqli_connect($host, $user, $password, $database) 
-    or die("Ошибка " . mysqli_error($link)); 
-$a =  mysqli_query($link,"SELECT COUNT(1) FROM users");
-$b = mysqli_fetch_array( $a );
-echo $b[0]; // выведет число строк
-?>
